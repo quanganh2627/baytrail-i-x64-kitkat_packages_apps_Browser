bbf1182ebd8ce7628b6b27fbb6c176b69bb47824
lwan89x <liangx.wang@intel.com>
2014-01-08 17:52:55 +0800

    [REVERT ME] Debug patch: Check ThreadPolicy in Browser app.
    BZ: 183200
    
    Change-Id: Ibcd12819daf20caf1adf5741c3b9737778788bd3
    Category: aosp improvement
    Domain: AOSP-Framework-CoreService
    Origin: internal
    Upstream-Candidate: no, need rework
    Signed-off-by: lwan89x <liangx.wang@intel.com>

--- src/com/android/browser/SuggestionsAdapter.java
+++ src/com/android/browser/SuggestionsAdapter.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.provider.BrowserContract;
 import android.text.Html;
 import android.text.TextUtils;
@@ -33,6 +34,7 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.util.Log;
 
 import com.android.browser.provider.BrowserProvider2.OmniboxSuggestions;
 import com.android.browser.search.SearchEngine;
@@ -521,6 +523,8 @@ public class SuggestionsAdapter extends BaseAdapter implements Filterable,
             if (mCursor != null) {
                 mCursor.close();
             }
+            Log.d("SuggestionsAdapter", "**BZ183200** (" + Thread.currentThread().getId() + ") runQuery: java threadpolicymask " + StrictMode.getThreadPolicyMask());
+            Log.d("SuggestionsAdapter", "**BZ183200** (" + Thread.currentThread().getId() + ") runQuery: native threadpolicymask " + StrictMode.getThreadNativePolicyMask());
             SearchEngine searchEngine = mSettings.getSearchEngine();
             if (!TextUtils.isEmpty(constraint)) {
                 if (searchEngine != null && searchEngine.supportsSuggestions()) {
