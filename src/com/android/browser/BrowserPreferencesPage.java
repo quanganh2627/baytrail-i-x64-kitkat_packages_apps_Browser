8f5d717fdcf9288a15becf7ed34ea26fa63615a0
buildslave <buildslave@buildbot.tl.intel.com>
2014-01-09 13:19:51 +0000

    "Automatically revert commits from previous weekly release"
    Change-Id: I11ca6b58150139aba0d7f51a2d0f40f6ecd4dad5
    Signed-off-by: buildslave <buildslave@buildbot.tl.intel.com>

--- src/com/android/browser/BrowserPreferencesPage.java
+++ src/com/android/browser/BrowserPreferencesPage.java
@@ -107,8 +107,7 @@ public class BrowserPreferencesPage extends PreferenceActivity {
             "com.android.browser.preferences.AccessibilityPreferencesFragment",
             "com.android.browser.preferences.AdvancedPreferencesFragment",
             "com.android.browser.preferences.BandwidthPreferencesFragment",
-            "com.android.browser.preferences.LabPreferencesFragment",
-            "com.android.browser.preferences.WebsiteSettingsFragment"));
+            "com.android.browser.preferences.LabPreferencesFragment"));
 
     @Override
     protected boolean isValidFragment(String fragmentName) {
